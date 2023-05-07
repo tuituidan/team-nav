a31008eaf72db0dcc71b9d137933d38b177bb80e
tuituidan <tuituidan@163.com>
2023-05-07 16:19:04 +0800

    倒计时报错解决，样式大小切换

--- src/main/java/com/tuituidan/openhub/bean/dto/CountdownDto.java
+++ src/main/java/com/tuituidan/openhub/bean/dto/CountdownDto.java
@@ -1,5 +1,7 @@
 package com.tuituidan.openhub.bean.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,5 +21,6 @@ public class CountdownDto {
 
     private String prefixText;
 
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime endTime;
 }
--- src/main/resources/static/assets/js/vue-utils.js
+++ src/main/resources/static/assets/js/vue-utils.js
@@ -39,7 +39,55 @@ CustomPlugin.install = Vue => {
                     ])
                 }
             });
-        }
+        },
+        /**
+         * 格式日期.
+         *
+         * @param {date} date 是否输出日志
+         * @param {string} fmt  模块名称
+         * @returns {string} 格式化后的字符串
+         */
+        formatDate(date, fmt) {
+            // 如果date为null、‘’或undefined直接返回为空
+            if (!date) {
+                return '';
+            }
+            let newDate = date;
+            let newFmt = fmt || 'yyyy-MM-dd';
+            if (!(newDate instanceof Date)) {
+                newDate = new Date(newDate);
+            }
+            const o = {
+                // 月份
+                'M+': newDate.getMonth() + 1,
+                // 日
+                'd+': newDate.getDate(),
+                // 小时
+                'H+': newDate.getHours(),
+                // 分
+                'm+': newDate.getMinutes(),
+                // 秒
+                's+': newDate.getSeconds(),
+                // 季度
+                'q+': Math.floor((newDate.getMonth() + 3) / 3),
+                // 毫秒
+                'S': newDate.getMilliseconds()
+            };
+            for (const time in o) {
+                if (isNaN(o[time])) {
+                    return '';
+                }
+            }
+            if (/(y+)/.test(newFmt)) {
+                newFmt = newFmt.replace(RegExp.$1, (String(newDate.getFullYear())).substr(4 - RegExp.$1.length));
+            }
+            for (const k in o) {
+                if (new RegExp(`(${k})`).test(newFmt)) {
+                    newFmt = newFmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : ((`00${o[k]}`).substr((`${o[k]}`).length)));
+                }
+            }
+            return newFmt;
+        },
     };
     Vue.prototype.$notice = {
         suc(msg) {
