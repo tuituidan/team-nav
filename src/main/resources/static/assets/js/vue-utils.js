00360a0129bede595182d39a6309c59a52313841
tuituidan <tuituidan@163.com>
2023-05-08 20:09:22 +0800

    分类图标选择和卡片图标选择后关闭弹窗

--- src/main/resources/static/assets/js/vue-utils.js
+++ src/main/resources/static/assets/js/vue-utils.js
@@ -121,3 +121,21 @@ CustomPlugin.install = Vue => {
     };
 };
 Vue.use(CustomPlugin);
+Vue.directive('clickoutside', {
+    bind(el, binding) {
+        function documentHandler(e) {
+            if (el.contains(e.target)) {
+                return false;
+            }
+            if (binding.expression) {
+                binding.value(e);
+            }
+        }
+        el.__vueClickOutside__ = documentHandler;
+        document.addEventListener('click', documentHandler);
+    },
+    unbind(el) {
+        document.removeEventListener('click', el.__vueClickOutside__);
+        delete el.__vueClickOutside__;
+    }
+});
